0b49d0eb6cb3e55e6c28b264675924d5453e3ab3
Koshake <koshake1@mail.com>
2021-11-19 23:36:08 +0300

    feature: animation

--- app/src/main/java/com/koshake1/carcontroller/CarView.kt
+++ app/src/main/java/com/koshake1/carcontroller/CarView.kt
@@ -1,12 +1,15 @@
 package com.koshake1.carcontroller
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlin.math.min
 
-private const val RADIUS_OFFSET_INDICATOR = -25
+private const val RADIUS_OFFSET_INDICATOR = -35
+private const val ANIMATION_DURATION = 2000L
+private const val RADIUS_COEF = 0.75
 
 class CarView@JvmOverloads constructor(
     context: Context,
@@ -18,10 +21,10 @@ class CarView@JvmOverloads constructor(
         isClickable = true
     }
 
+    private lateinit var animator: ValueAnimator
     private var radius = 0.0f
-    private var angle = 0
+    private var angle = 0.0f
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
-
     private val carBitMap = BitmapFactory.decodeResource(context.resources, R.drawable.car_blue)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -30,16 +33,14 @@ class CarView@JvmOverloads constructor(
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        radius = (min(width, height) / 2.0 * 0.8).toFloat()
+        radius = (min(width, height) / 2.0 * RADIUS_COEF).toFloat()
+        initAnimator()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
-        canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
-
         val markerRadius = radius + RADIUS_OFFSET_INDICATOR
-        pointPosition.computeXYForSpeed(angle, markerRadius)
+        pointPosition.computeXY(angle, markerRadius)
         paint.color = Color.BLACK
         canvas?.drawBitmap(carBitMap, pointPosition.x, pointPosition.y, paint)
     }
@@ -47,16 +48,23 @@ class CarView@JvmOverloads constructor(
     override fun performClick(): Boolean {
         if (super.performClick()) return true
 
-        angle++
-        invalidate()
+        animator.start()
         return true
 
     }
 
-    private fun PointF.computeXYForSpeed(pos: Int, radius: Float) {
-        val startAngle = Math.PI/4
-        val angle = startAngle + pos * (Math.PI / 4)
+    private fun PointF.computeXY(pos: Float, radius: Float) {
+        val angle = angle + pos * (Math.PI / 4)
         x = (radius * kotlin.math.cos(angle)).toFloat() + width / 2
         y = (radius * kotlin.math.sin(angle)).toFloat() + height / 2
+    }
+
+    private fun initAnimator() {
+        animator = ValueAnimator.ofFloat(0f, 10f)
+        animator.duration = ANIMATION_DURATION
+        animator.addUpdateListener {
+            angle = it.animatedValue as Float
+            invalidate()
+        }
     }
 }
